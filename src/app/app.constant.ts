702ff6015111682d14f19f943df6df48bc953531
Rushikesh1712 <uvrushikesh@gmail.com>
2025-09-29 16:16:01 +0530

    Link changes

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,18 +1,5 @@
 export const appkeys = {
   gmUrl: 'http://gm.tecpool.in:8079/',
-
-  // // Live
-  // baseUrl: 'http://12dimension.uvtechsoft.com:9889/',
-  // retriveimgUrl: 'http://12dimension.uvtechsoft.com:9889/static/',
-
-  // // // // Demo Dimension
   baseUrl: 'https://admin.12dimensionsapp.in:9878/',
   retriveimgUrl: 'https://admin.12dimensionsapp.in:9878/static/',
-
-  // Local sumit
-  // baseUrl: 'https://wnm884z0-2003.inc1.devtunnels.ms/',
-  // retriveimgUrl: 'https://wnm884z0-2003.inc1.devtunnels.ms/static/',
-
-  // baseUrl: 'http://192.168.29.246:2003/',
-  // retriveimgUrl: 'http://192.168.29.246:2003/static/',
 };
